7ee4ec33491b4249054c229e0b79ce8c1413240d
Miguel Sanchez (Note) <sanchez.pablo.miguel@gmail.com>
2016-07-06 19:56:33 -0300

    Bug fixed

--- Prototype001/src/relytest/ui/common/ScreenPrinter.java
+++ Prototype001/src/relytest/ui/common/ScreenPrinter.java
@@ -3,9 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package relytest.ui.common;
+
 import java.awt.Dimension;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
@@ -13,29 +15,30 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import javax.imageio.ImageIO;
 import relytest.interfaces.IScreenPrinter;
+
 /**
  *
  * @author Gabriela Sanchez - Miguel Sanchez
  */
-public class ScreenPrinter implements IScreenPrinter{
-    private final String PicFormat =".jpg";
+public class ScreenPrinter implements IScreenPrinter {
+
+    private final String PicFormat = ".jpg";
+
     @Override
-    public String print(String path) {  
+    public String print(String path) {
         try {
-
-            Toolkit tool = Toolkit.getDefaultToolkit();
-            Dimension d = tool.getScreenSize();
-            Rectangle rect = new Rectangle(d);
+            Rectangle screenRect = new Rectangle(0, 0, 0, 0);
+            for (GraphicsDevice gd : GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()) {
+                screenRect = screenRect.union(gd.getDefaultConfiguration().getBounds());
+            }
             Robot robot = new Robot();
-            Thread.sleep(1000);                  
-            File f = new File(path+ PicFormat);
-            BufferedImage img = robot.createScreenCapture(rect);
+            Thread.sleep(1000);
+            File f = new File(path + PicFormat);
+            BufferedImage img = robot.createScreenCapture(screenRect);
             ImageIO.write(img, "jpeg", f);
-            tool.beep();
-
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return PicFormat;           
-    }   
+        return PicFormat;
+    }
 }
